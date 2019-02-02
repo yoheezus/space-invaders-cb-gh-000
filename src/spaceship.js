4102a28ccca9dd261c838daf6d1b1a193ab5cfd6
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 12:14:23 +0000

    Automatically backed up by Learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -12,7 +12,7 @@ class Spaceship {
     }
 
     notifyCrew() {
-        for (let i = 0; i < this.crew.length; i++; {
+        for (let i = 0; i < this.crew.length; i++) {
             this.crew[i].currentShip = this
         }
     }
