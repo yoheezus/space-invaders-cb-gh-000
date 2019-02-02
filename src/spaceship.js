ade9711db8eef2d620fd4fdc00fb9d90e1c3f126
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 12:03:19 +0000

    Automatically backed up by Learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -7,7 +7,7 @@ class Spaceship {
         this.cloaked = false
         this.warpDrive = "disengaged"
         this.phasersCharge = "uncharged"
-        if (!crew) {
+        if (!this.crew) {
             this.docked = false
         } else {
             this.docked = true
