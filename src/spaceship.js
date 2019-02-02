18ff18f6855463b2865119966425d8effeac4349
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 12:19:43 +0000

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,19 @@
-class Spaceship {}
+class Spaceship {
+    constructor(name, crew, phasers, shields) {
+        this.name = name;
+        this.crew = crew;
+        this.phasers = phasers;
+        this.shields = shields;
+        this.cloaked = false;
+        this.warpDrive = "disengaged";
+        this.phasersCharge = "uncharged";
+        crew.length ? (this.docked = false) : (this.docked = true);
+        this.notifyCrew();
+    }
+
+    notifyCrew() {
+        for (let i = 0; i < this.crew.length; i++) {
+            this.crew[i].currentShip = this
+        }
+    }
+}
