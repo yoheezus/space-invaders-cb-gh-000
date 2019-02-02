9b7a7ebf2d206731adf734b90e68d9cdf86337d3
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 12:09:46 +0000

    Automatically backed up by Learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,13 +1,13 @@
 class Spaceship {
     constructor(name, crew, phasers, shields) {
-        this.name = name
-        this.crew = crew
-        this.phasers = phasers
-        this.shields = shields
-        this.cloaked = false
-        this.warpDrive = "disengaged"
-        this.phasersCharge = "uncharged"
-        crew.length ? (this.docked = false) : (this.docked = true)
+        this.name = name;
+        this.crew = crew;
+        this.phasers = phasers;
+        this.shields = shields;
+        this.cloaked = false;
+        this.warpDrive = "disengaged";
+        this.phasersCharge = "uncharged";
+        crew.length ? (this.docked = false) : (this.docked = true);
         this.notifyCrew();
     }
 
